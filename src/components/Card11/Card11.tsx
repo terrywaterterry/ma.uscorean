4ea7b27fcf05f8d43a3ca2483ddfd0e70c285364
Sam is Good <samisgood@Samui-MacBookAir-2.local>
2026-01-26 15:05:36 -0500

    remove futured image on card 11

--- src/components/Card11/Card11.tsx
+++ src/components/Card11/Card11.tsx
@@ -45,7 +45,7 @@ const Card11: FC<Card11Props> = ({
 				className={`relative z-10 block w-full flex-shrink-0 overflow-hidden rounded-t-3xl ${ratio}`}
 			>
 				<div>
-					<PostFeaturedMedia post={post} isHover={isHover} />
+					<!--<PostFeaturedMedia post={post} isHover={isHover} />-->
 				</div>
 			</div>
 			<Link href={uri || ''} className="absolute inset-0"></Link>
